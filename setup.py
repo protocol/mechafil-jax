b8c9df9a27812fcbfc2918e7a6ddffd68e63b019
Kiran Karra <kiran.karra@gmail.com>
2023-09-05 12:04:13 -0700

    fixing links for py setup

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ setuptools.setup(
         "numpy", 
         "scipy", 
         "matplotlib",
-        "pystarboard @ git+ssh://git@github.com:protocol/pystarboard.git",
+        "pystarboard @ git+https://git@github.com/protocol/pystarboard.git",
     ],
 
     tests_require = [
         'pytest',
         'pandas==1.5.3',
-        'mechaFIL @ git+ssh://git@github.com:protocol/filecoin-mecha-twin.git@mechafil_jax'  # get the branch that is built for comparisons w/ jax
+        'mechaFIL @ git+https://git@github.com:protocol/filecoin-mecha-twin.git@mechafil_jax'  # get the branch that is built for comparisons w/ jax
     ]
 )
